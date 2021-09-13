e802b8abaa71fb998e98b72cfc9ef29c96a6e2d5
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2021-09-13 21:37:53 +0200

    Created backend project, able to connect to local mysql db

---
+++ Backend/src/main/java/Application.java
@@ -0,0 +1,47 @@
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class Application {
+    Connection con;
+
+    Application() {
+        this.connectToDB();
+        if (con == null) {
+            System.out.println("Can't connect to server...");
+        }
+    }
+
+    private void connectToDB() {
+        MysqlDataSource ds = getMySQLDataSource();
+
+            try {
+                con = ds.getConnection();
+                System.out.println("Connected");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+    }
+    private static MysqlDataSource getMySQLDataSource() {
+        //Keeping properties in another folder for username and so on...
+        Properties props = new Properties();
+        //Path for the file
+        String fileName = "src/main/resources/db.properties";
+
+        try (FileInputStream in = new FileInputStream(fileName)) {
+            props.load(in);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        //Creating datasource for better performance and scalability.
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setURL(props.getProperty("mysql.url"));
+        ds.setUser(props.getProperty("mysql.username"));
+        ds.setPassword(props.getProperty("mysql.password"));
+
+        return ds;
+    }
+}
