f77e5a38d7368e42b64e16208a7460be2a39da0b
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2021-09-15 22:05:25 +0200

    Setting up java express app to the application. And adding proxy port 4000 in package.json so we can talk with the frontend

--- Backend/src/main/java/Main.java
+++ Backend/src/main/java/Main.java
@@ -1,6 +1,11 @@
+import express.Express;
+
 public class Main {
 
     public static void main(String[] args) {
-        new Application();
+        Express app = new Express();
+        new ConnectMysql();
+
+        app.listen(4000);
     }
 }
