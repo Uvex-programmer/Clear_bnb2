a38fba54621150a090d65c81c8f0f8e374b1a126
Martin-Persson <jm-persson@hotmail.com>
2021-09-19 22:03:48 +0200

    ...

--- Backend/src/main/java/repositories/PropertyRepository.java
+++ Backend/src/main/java/repositories/PropertyRepository.java
@@ -2,44 +2,45 @@ package repositories;
 
 import express.Express;
 import models.Property;
+
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 public class PropertyRepository {
-
+    
     private final EntityManager entityManager;
     private Express app;
-
+    
     public PropertyRepository(EntityManager entityManager, Express app) {
         this.entityManager = entityManager;
         this.app = app;
         this.propertyMethods();
     }
-
-    public void propertyMethods(){
+    
+    public void propertyMethods() {
         app.post("/api/addProperty", (req, res) -> {
             Property property = req.body(Property.class);
             save(property);
         });
     }
-
+    
     public Optional<Property> findById(Integer id) {
         Property property = entityManager.find(Property.class, id);
         return property != null ? Optional.of(property) : Optional.empty();
     }
-
-    public List findAll() {
+    
+    public List<?> findAll() {
         return entityManager.createQuery("from Property").getResultList();
     }
-
+    
     public Optional<Property> findByName(String name) {
         Property property = entityManager.createQuery("SELECT p FROM properties p WHERE p.title = :name", Property.class)
                 .setParameter("name", name)
                 .getSingleResult();
         return property != null ? Optional.of(property) : Optional.empty();
     }
-
+    
     public Optional<Property> save(Property property) {
         try {
             entityManager.getTransaction().begin();
@@ -51,4 +52,6 @@ public class PropertyRepository {
         }
         return Optional.empty();
     }
+    
+    
 }
