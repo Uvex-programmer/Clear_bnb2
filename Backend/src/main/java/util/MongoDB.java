5c4cc912b0a638c20a2536638d8c97e4950db837
Martin-Persson <jm-persson@hotmail.com>
2021-09-29 22:16:18 +0200

    checking if cache is updated

--- Backend/src/main/java/util/MongoDB.java
+++ Backend/src/main/java/util/MongoDB.java
@@ -8,8 +8,7 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import repositories.PropertyRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
@@ -19,6 +18,7 @@ public class MongoDB {
     static MongoCollection<PropertyView> collection;
     private final PropertyRepository propertyRepository;
     MongoDatabase database;
+    static Set<Integer> idNumbers = new HashSet<>();
 
 
     public MongoDB(PropertyRepository propertyRepository) {
@@ -38,23 +38,30 @@ public class MongoDB {
     public static List<PropertyView> checkIfCached(List<PropertyView> properties) {
         FindIterable<PropertyView> mongoCollection = collection.find();
         List<PropertyView> propertyViews = convertToList(mongoCollection);
-        //Ändra datum till timestamps så att man kan jämnföra mongo och sql.
-        propertyViews.forEach(System.out::println);
-        properties.forEach(System.out::println);
-        return propertyViews;
-
+    
+        if(properties.stream().anyMatch(p -> idNumbers.contains(p.getId())) && idNumbers.size() == properties.size()){
+            System.out.println("From mongo");
+            return propertyViews;
+        }else{
+            System.out.println("From SQL");
+            return properties;
+        }
     }
 
     private void populateCache() {
         collection.deleteMany(new Document());
-        collection.insertMany(propertyRepository.findAvailableObjects());
+        List<PropertyView> availableObjects = propertyRepository.findAvailableObjects();
+        
+        idNumbers = new HashSet<>();
+        availableObjects.forEach(obj -> idNumbers.add(obj.getId()));
+        System.out.println(idNumbers);
+        
+        collection.insertMany(availableObjects);
     }
 
     public void connectToMongoDB() {
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
                 fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-
-
         String uri = "mongodb+srv://Slobban:1234@cluster0.q0kct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
         MongoClient mongoClient = MongoClients.create(uri);
