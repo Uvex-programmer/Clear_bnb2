807a67576dc484e4f2b0f4931fc0429300ee2ac1
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2021-09-30 21:22:08 +0200

    merge bit by bit

---
+++ Frontend/clear_bnb/src/components/AddProperty/AddProperty.js
@@ -0,0 +1,152 @@
+import './AddProperty.css'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import Amenities from '../Amenities/Amenities'
+
+export default function AddProperty() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [address, setAddress] = useState('')
+  const [zipcode, setZipcode] = useState('')
+  const [city, setCity] = useState('')
+  const [beds, setBeds] = useState('')
+  const [bathrooms, setBathrooms] = useState('')
+  const [guests, setGuests] = useState('')
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
+  const [price, setPrice] = useState('')
+
+  const userOnline = useSelector((state) => state.loginUser.user)
+  let amenitiesAdd = []
+  let history = useHistory()
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    let propertyObj = {
+      description: description,
+      title: title,
+      beds: beds,
+      bathrooms: bathrooms,
+      guests: guests,
+      startDate: startDate,
+      endDate: endDate,
+      dailyPrice: price,
+      address: {
+        street: address,
+        zipcode: zipcode,
+        city: city,
+      },
+      user: {
+        id: userOnline.id,
+        firstName: userOnline.firstName,
+        lastName: userOnline.lastName,
+        email: userOnline.email,
+      },
+      amenities: amenitiesAdd,
+    }
+
+    console.log(`propertyObj`, propertyObj)
+
+    await fetch('/api/add-property', {
+      method: 'POST',
+      body: JSON.stringify(propertyObj),
+    }).then(
+      setTimeout(() => {
+        history.push('/')
+      }, 1000)
+    )
+  }
+
+  const pushOrDelete = (type) => {
+    if (amenitiesAdd.some((e) => e.amenity === type)) {
+      return (amenitiesAdd = amenitiesAdd.filter(
+        (item) => item.amenity !== type
+      ))
+    } else {
+      amenitiesAdd.push({ amenity: type })
+    }
+  }
+
+  return (
+    <>
+      <div className='add-property-container'>
+        <form onSubmit={submitHandler}>
+          <label>Title</label>
+          <input
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Small description</label>
+          <input
+            type='text'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <label>Address</label>
+          <input
+            type='text'
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <label>Zipcode</label>
+          <input
+            type='text'
+            value={zipcode}
+            onChange={(e) => setZipcode(e.target.value)}
+          />
+          <label>City</label>
+          <input
+            type='text'
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <label>Number of beds</label>
+          <input
+            type='text'
+            value={beds}
+            onChange={(e) => setBeds(e.target.value)}
+          />
+          <label>Number of bathrooms</label>
+          <input
+            type='text'
+            value={bathrooms}
+            onChange={(e) => setBathrooms(e.target.value)}
+          />
+          <label>Max guests</label>
+          <input
+            type='text'
+            value={guests}
+            onChange={(e) => setGuests(e.target.value)}
+          />
+          <label>Start date</label>
+          <input
+            type='date'
+            min={Date.now()}
+            max='2023-01-01'
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+          <label>End date</label>
+          <input
+            type='date'
+            min={Date()}
+            max='2023-01-01'
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+          <label>Price per night</label>
+          <input
+            type='text'
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <Amenities pushOrDelete={pushOrDelete} />
+          <button onClick={(e) => submitHandler(e)}>Save</button>
+        </form>
+      </div>
+    </>
+  )
+}
