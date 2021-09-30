87eb6d78041f27f5c6fea509eeee13a7b09a26ba
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2021-09-30 20:14:34 +0200

    Added addresLogic, Mapper, DTO

---
+++ Backend/src/main/java/DTO/AddressDTO.java
@@ -0,0 +1,67 @@
+package DTO;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import models.Property;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+public class AddressDTO {
+
+    private int id;
+    private String street;
+    private String zipcode;
+    private String city;
+    private Property property;
+
+    public AddressDTO() {
+    }
+
+    public AddressDTO(int id, String street, String zipcode, String city, Property property) {
+        this.id = id;
+        this.street = street;
+        this.zipcode = zipcode;
+        this.city = city;
+        this.property = property;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+}
