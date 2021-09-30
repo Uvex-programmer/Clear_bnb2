b9458c90fb6a8585125f209c359d6884e79b36d5
Martin Persson <jm-persson@hotmail.com>
2021-09-30 11:43:35 +0200

    Added user bank account upon creation of user. (fixed value of 10000)

--- Backend/src/main/java/models/BankAccount.java
+++ Backend/src/main/java/models/BankAccount.java
@@ -12,43 +12,43 @@ public class BankAccount {
     private int id;
     private double funds;
 
-    @JsonBackReference(value="User - Account")
+    @JsonBackReference(value = "User - Account")
     @OneToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
-    
+
     public BankAccount() {
     }
-    
+
     public BankAccount(double funds) {
         this.funds = funds;
     }
-    
-    
+
+
     public int getId() {
         return id;
     }
-    
+
     public void setId(int id) {
         this.id = id;
     }
-    
+
     public double getFunds() {
         return funds;
     }
-    
+
     public void setFunds(double funds) {
         this.funds = funds;
     }
-    
+
     public User getUser() {
         return user;
     }
-    
+
     public void setUser(User user) {
         this.user = user;
     }
-    
+
     @Override
     public String toString() {
         return "BankAccount{" +
