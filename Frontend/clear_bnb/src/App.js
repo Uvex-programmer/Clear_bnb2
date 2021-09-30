decdcdc507b34bb65defeb14e1420ee0fba3e727
Dannemaniman <daniellangas91@gmail.com>
2021-09-30 12:23:07 +0200

    Changed formatting in app.js

--- Frontend/clear_bnb/src/App.js
+++ Frontend/clear_bnb/src/App.js
@@ -13,41 +13,41 @@ import { useDispatch } from 'react-redux'
 import { login } from './slicers/LoginSlicer'
 
 function App() {
-	const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-	useEffect(() => {
-		fetch('/api/whoami')
-			.then(async (res) => await res.json())
-			.then((user) => {
-				if (!user) return console.log('No user currently logged in.')
+  useEffect(() => {
+    fetch('/api/whoami')
+      .then(async (res) => await res.json())
+      .then((user) => {
+        if (!user) return console.log('No user currently logged in.')
 
-				const userLoggedIn = {
-					id: user.id,
-					firstName: user.firstName,
-					lastName: user.lastName,
-					email: user.email,
-				}
-				dispatch(login(userLoggedIn))
-				console.log('user logged in: ', user)
-			})
-	}, [dispatch])
+        const userLoggedIn = {
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+        }
+        dispatch(login(userLoggedIn))
+        console.log('user logged in: ', user)
+      })
+  }, [dispatch])
 
-	return (
-		<div id='App'>
-			<Navbar />
-			<div className='app-container'>
-				<Switch>
-					<Route exact path='/' component={FrontPage} />
-					<Route path='/login' component={Login} />
-					<Route path='/add-property' component={AddProperty} />
-					<Route path='/search' component={SearchPage} />
-					<Route path='/profile-page/:id' component={ProfilePage} />
-					<Route path='/detail-page/:id' component={Detailpage} />
-					<Route path='/booking/:id' component={BookingPage} />
-				</Switch>
-			</div>
-		</div>
-	)
+  return (
+    <div id='App'>
+      <Navbar />
+      <div className='app-container'>
+        <Switch>
+          <Route exact path='/' component={FrontPage} />
+          <Route path='/login' component={Login} />
+          <Route path='/add-property' component={AddProperty} />
+          <Route path='/search' component={SearchPage} />
+          <Route path='/profile-page/:id' component={ProfilePage} />
+          <Route path='/detail-page/:id' component={Detailpage} />
+          <Route path='/booking/:id' component={BookingPage} />
+        </Switch>
+      </div>
+    </div>
+  )
 }
 
 export default App
