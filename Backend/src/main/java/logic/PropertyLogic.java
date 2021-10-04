62a9060118e0ff1ef3aba7f0db962983de35e1de
Martin-Persson <jm-persson@hotmail.com>
2021-10-04 08:54:39 +0200

    adding new property to cache when created

--- Backend/src/main/java/logic/PropertyLogic.java
+++ Backend/src/main/java/logic/PropertyLogic.java
@@ -3,68 +3,75 @@ package logic;
 import DTO.PropertyDTO;
 import mapper.LogMapper;
 import mapper.PropertyMapper;
-import models.*;
+import models.Property;
+import models.PropertyLog;
+import models.PropertyView;
 import repositories.PropertyRepository;
+import util.MongoDB;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class PropertyLogic {
-
+    
     PropertyRepository propertyRepository = new PropertyRepository();
     PropertyMapper propertyMapper = new PropertyMapper();
     LogMapper logMapper = new LogMapper();
-
-    public Property addProperty(Property property){
-        property.setDailyPrice((int) Math.ceil(property.getDailyPrice()*1.15));
+    
+    public Property addProperty(Property property) {
+        property.setDailyPrice((int) Math.ceil(property.getDailyPrice() * 1.15));
         property.addAddress(property.getAddress());
         property.addUser(property.getUser());
         property.addAmenities(property.getAmenities());
         System.out.println(property);
         propertyRepository.save(property);
+        MongoDB.insertProperty(property);
         return property;
     }
-
-    public List<PropertyDTO> getProperties(){
+    
+    public List<PropertyDTO> getProperties() {
+        //mongo db
+        System.out.println(propertyRepository.findAvailableObjects());
+        MongoDB.checkIfCached(propertyRepository.findAvailableObjects());
         List<Property> properties = propertyRepository.findAvailableObjects();
-        if(properties.isEmpty()) return null;
+        if (properties.isEmpty()) return null;
         ArrayList<PropertyDTO> propertiesDTOs = new ArrayList<>();
-        for(Property p : properties){
+        for (Property p : properties) {
             propertiesDTOs.add(propertyMapper.propertyToDTO(Optional.ofNullable(p)));
         }
         return propertiesDTOs;
     }
-
-    public PropertyDTO getProperty(Integer id){
-       Optional<Property> property = propertyRepository.findById(id);
-       if(property.isEmpty()) return null;
-
+    
+    public PropertyDTO getProperty(Integer id) {
+        Optional<Property> property = propertyRepository.findById(id);
+        if (property.isEmpty()) return null;
+        
         return propertyMapper.propertyToDTO(property);
     }
-
-    public ArrayList<PropertyDTO> getUserProperties(Integer id){
+    
+    public ArrayList<PropertyDTO> getUserProperties(Integer id) {
         List<Property> properties = propertyRepository.findByUserId(id);
         ArrayList<PropertyDTO> propertiesDTO = new ArrayList<>();
-        for(Property p : properties){
+        for (Property p : properties) {
             propertiesDTO.add(propertyMapper.propertyToDTO(Optional.ofNullable(p)));
         }
         return propertiesDTO;
     }
-
-    public List<?> searchProperties(PropertyView searchResult){
+    
+    public List<?> searchProperties(PropertyView searchResult) {
         return propertyRepository.findObjectsBySearch(searchResult.getCity(),
                 searchResult.getBeds(), searchResult.getBaths(), searchResult.getGuests(), searchResult.getDailyPrice(),
                 searchResult.getStartDate(), searchResult.getEndDate());
     }
-
-    public void updateProperty(PropertyDTO p, Integer id){
+    
+    public void updateProperty(PropertyDTO p, Integer id) {
         Optional<Property> propertyBefore = propertyRepository.findById(id);
         Property property = propertyMapper.dtoToProperty(p, propertyBefore);
         PropertyLog propertyLog = logMapper.propertyToLog(propertyBefore);
         property.getPropertyLogs().add(propertyLog);
         propertyRepository.updateProperty(property);
     }
-
-
+    
+    
 }
--- Backend/src/main/java/util/MongoDB.java
+++ Backend/src/main/java/util/MongoDB.java
@@ -3,31 +3,33 @@ package util;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.*;
 import models.Property;
-import models.PropertyView;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import repositories.PropertyRepository;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class MongoDB {
-
+    
     static MongoCollection<Property> collection;
+    static Set<Integer> idNumbers = new HashSet<>();
     private final PropertyRepository propertyRepository;
     MongoDatabase database;
-    static Set<Integer> idNumbers = new HashSet<>();
-
-
+    
+    
     public MongoDB(PropertyRepository propertyRepository) {
         this.propertyRepository = propertyRepository;
         this.connectToMongoDB();
         populateCache(propertyRepository);
     }
-
+    
     public static List<Property> convertToList(FindIterable<Property> mongoCollection) {
         List<Property> list = new ArrayList<>();
         for (Property item : mongoCollection) {
@@ -35,38 +37,39 @@ public class MongoDB {
         }
         return list;
     }
-
+    
     public static List<Property> checkIfCached(List<Property> properties) {
         FindIterable<Property> mongoCollection = collection.find();
         List<Property> mongoDbProperties = convertToList(mongoCollection);
-    
-        if(properties.stream().anyMatch(p -> idNumbers.contains(p.getId())) && idNumbers.size() == properties.size()){
+        
+        if (properties.stream().anyMatch(p -> idNumbers.contains(p.getId())) && idNumbers.size() == properties.size()) {
             System.out.println("From mongo");
             return mongoDbProperties;
-        }else{
+        } else {
             System.out.println("From SQL");
             return properties;
         }
     }
-
+    
     public static void populateCache(PropertyRepository propertyRepository) {
         collection.deleteMany(new Document());
         List<Property> availableObjects = propertyRepository.findAvailableObjects();
         
-//        idNumbers = new HashSet<>();
         availableObjects.forEach(obj -> idNumbers.add(obj.getId()));
-        System.out.println(idNumbers);
-        System.out.println(idNumbers.size());
-        System.out.println(availableObjects.size());
         
         collection.insertMany(availableObjects);
     }
-
+    
+    public static void insertProperty(Property property) {
+        idNumbers.add(property.getId());
+        collection.insertOne(property);
+    }
+    
     public void connectToMongoDB() {
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
                 fromProviders(PojoCodecProvider.builder().automatic(true).build()));
         String uri = "mongodb+srv://Slobban:1234@cluster0.q0kct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+        
         MongoClient mongoClient = MongoClients.create(uri);
         database = mongoClient.getDatabase("bnb-cache").withCodecRegistry(pojoCodecRegistry);
         collection = database.getCollection("Property", Property.class);
