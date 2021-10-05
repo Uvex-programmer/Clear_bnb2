0f57b19015bb8853fc6ad0b1f32964239c234ab0
Dannemaniman <daniellangas91@gmail.com>
2021-10-05 18:23:07 +0200

    Added constructor to CustomerSupport class and also encapsulated field variables with private access modifier, also added getters and setters.

--- Backend/src/main/java/util/CustomerSupport.java
+++ Backend/src/main/java/util/CustomerSupport.java
@@ -4,12 +4,36 @@ import io.javalin.websocket.WsContext;
 import java.util.List;
 
 public class CustomerSupport implements Observer {
-    WsContext worker; // = new ArrayList<>();
+    private WsContext worker;
+    private String id;
 
-    public void add(WsContext ctx) { this.worker = ctx; }
+
+    public CustomerSupport(WsContext worker, String id) {
+        this.worker = worker;
+        this.id = id;
+    }
+
+
 
     @Override
-    public void onUpdate(List<?> chatrooms) {
-        worker.send(chatrooms);
+    public List<?> onUpdate(List<?> chatRooms) {
+        return chatRooms;
+    }
+
+
+    public WsContext getWorker() {
+        return worker;
+    }
+
+    public void setWorker(WsContext worker) {
+        this.worker = worker;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
