c4022c55b97e8ef3676bfdaa7ef2d2bffd91603f
Dannemaniman <daniellangas91@gmail.com>
2021-10-06 04:30:37 +0200

    Refactored and cleaned up SocketRoutes, also removed ctx from the chatroomss, in case user lost connection or errored.

--- Backend/src/main/java/routes/SocketRoutes.java
+++ Backend/src/main/java/routes/SocketRoutes.java
@@ -1,14 +1,7 @@
 package routes;
 
 import express.Express;
-import io.javalin.websocket.WsContext;
 import logic.SocketLogic;
-import models.Message;
-import java.util.List;
-import java.util.Map;
-
-import static logic.SocketLogic.chatRoomExists;
-import static logic.SocketLogic.isAdmin;
 
 public class SocketRoutes {
     private final Express app;
@@ -21,29 +14,21 @@ public class SocketRoutes {
 
     public void socketMethods() {
         app.ws("/websockets/chat", ws -> {
-            ws.onConnect(ctx -> { ctx.send(socketLogic.onConnectHandler(ctx, ctx.cookie("current-user"))); });
+            ws.onConnect(ctx -> ctx.send(socketLogic.onConnectHandler(ctx, ctx.cookie("current-user"))));
             ws.onMessage(ctx -> {
-                Message msg = socketLogic.onMessageHandler(ctx);
-                String room_id = ctx.message(Message.class).getChatroom_id();
-                Map<String, List<WsContext>> chatRooms = socketLogic.getChatRooms();
-                if(isAdmin(room_id)) {
-                    msg.setIs_support(true);
-                    List<WsContext> chatRoom = socketLogic.getUsersFromRoom(room_id);
-                    if(!chatRoom.contains(ctx)) { socketLogic.addToRoom(room_id, ctx); }
-
-                    socketLogic.getUsersFromRoom(room_id).forEach(client -> { client.send(msg); });
-                }
-                else if (chatRoomExists(chatRooms, ctx)) {
-                    socketLogic.createChatRoom(ctx, msg).forEach(worker -> { worker.send(msg); });
-                    ctx.send(msg);
-                }
-                else {
-                    chatRooms.get(ctx.cookie("id")).forEach((participant) -> { participant.send(msg); });
-                }
+                socketLogic.onMessageHandler(ctx).forEach((msg, list) -> {
+                    list.forEach(participant -> participant.send(msg));
+                });
             });
             ws.onBinaryMessage(ctx -> System.out.println("Message"));
-            ws.onClose(ctx -> ctx.send("Close."));
-            ws.onError(ctx -> ctx.send("Error."));
+            ws.onClose(ctx -> {
+                socketLogic.removeUserFromRoom(ctx);
+                ctx.send("Close.");
+            });
+            ws.onError(ctx ->{
+                socketLogic.removeUserFromRoom(ctx);
+                ctx.send("Errored.");
+            });
         });
     }
 }
